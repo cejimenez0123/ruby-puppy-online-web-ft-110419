0dd1fab7fde1eaeee1681b81df948c6a3c9f90d7
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-16 22:06:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,10 +16,10 @@ class Dog
    def self.clear_all
       @@all.clear
    end
-   def print_all
+   def self.print_all
       @@all.each do |name|
         puts name
-      end
+    end
    end
    def save
       @@all.push(self)
