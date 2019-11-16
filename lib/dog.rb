3142f6c29d99ddb22eb15519e0751bd347c23e55
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-16 21:56:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 # Add your code here
 class Dog
   @@all = []
+  @@dog_instance= []
    def initialize(name)
      @name = name
    end
    def name
      @name
+   end
+
+   def self.all
+
    end
    def save
        @@all.push(self)
